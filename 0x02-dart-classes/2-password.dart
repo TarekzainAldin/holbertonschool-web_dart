330d79027841b676df38be4a3c1fd04a218bd9e7
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-07-26 10:55:55 +0200

    password with valid and has upper lower and number

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,17 @@
+class Password {
+    String password = '';
+    
+    bool isValid () {
+        if (password.length <8 || password.length >16) return false;
+        
+        bool hasUpperCase = password.contains(RegExp(r'[A-Z]'));
+        bool hasLowerCase = password.contains(RegExp(r'[a-z]'));
+        bool hasNumber = password.contains(RegExp(r'[0-9]'));
+        return hasUpperCase && hasLowerCase && hasNumber;
+
+    }
+    @override 
+    String toString(){
+        return ('your password is : $password');
+    }
+}
