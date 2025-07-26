acfd44c004b4ff9a34b960fc7a853908222bbfb4
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-07-26 11:15:45 +0200

    test

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,17 +1,18 @@
 class Password {
-    String password = '';
-    
-    bool isValid () {
-        if (password.length <8 || password.length >16) return false;
-        
-        bool hasUpperCase = password.contains(RegExp(r'[A-Z]'));
-        bool hasLowerCase = password.contains(RegExp(r'[a-z]'));
-        bool hasNumber = password.contains(RegExp(r'[0-9]'));
-        return hasUpperCase && hasLowerCase && hasNumber;
+  String password = '';
 
-    }
-    @override 
-    String toString(){
-        return ('your password is : $password');
-    }
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) return false;
+    
+    bool hasUpperCase = password.contains(RegExp(r'[A-Z]'));
+    bool hasLowerCase = password.contains(RegExp(r'[a-z]'));
+    bool hasNumber = password.contains(RegExp(r'[0-9]'));
+
+    return hasUpperCase && hasLowerCase && hasNumber;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
 }
