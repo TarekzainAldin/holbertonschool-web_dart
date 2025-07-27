51353a1869c1e66cccafa9d1ebcc926a65f3f233
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-07-27 20:44:14 +0200

    now

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,11 +1,10 @@
 import '0-util.dart';
 
-future<void> userCount() async {
-    try{
-        int count = await fetchUsersCount();
-        print(count);
-
-    }catch(e){
-        print('Error: $e');
-    }
-}
+Future<void> usersCount() async {
+  try {
+    int count = await fetchUsersCount();
+    print(count);
+  } catch (e) {
+    print('Error: $e');
+  }
+} 
--- 0x03-asynchronous/0-util.dart
+++ 0x03-asynchronous/0-util.dart
@@ -2,7 +2,4 @@ Future<int> fetchUsersCount() => Future.delayed(
       const Duration(seconds: 2),
       () => 19,
     );
-
-achref@achref:~$ dart 0-main.dart
-19
-achref@achref:~$
+    
