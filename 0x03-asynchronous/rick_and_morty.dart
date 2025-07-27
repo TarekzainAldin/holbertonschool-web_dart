5c121dac4a436317bd63c5ebcffbe92e1c05ac2a
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-07-27 21:38:04 +0200

    dfsf

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacters() async {
+  try {
+    final response = await http.get(
+      Uri.parse('https://rickandmortyapi.com/api/character'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final characters = data['results'] as List;
+      
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      throw 'Failed to fetch characters';
+    }
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
